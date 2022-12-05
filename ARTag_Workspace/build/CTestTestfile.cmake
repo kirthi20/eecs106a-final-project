967c086409d6217d2f293ef72ca74183bdc751eb
Class Account <ee106a-agf@c105-1.local>
2022-12-05 11:00:30 -0800

    added bundles! slay

--- ARTag_Workspace/build/CTestTestfile.cmake
+++ ARTag_Workspace/build/CTestTestfile.cmake
@@ -6,5 +6,6 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
 subdirs("ar_track_alvar/ar_track_alvar_msgs")
+subdirs("automatic_navigation")
 subdirs("ar_track_alvar/ar_track_alvar")
 subdirs("lab4_cam")
---
+++ ARTag_Workspace/build/automatic_navigation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa22/class/ee106a-agf/eecs106a-final-project/ARTag_Workspace/src/automatic_navigation
+# Build directory: /home/cc/ee106a/fa22/class/ee106a-agf/eecs106a-final-project/ARTag_Workspace/build/automatic_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
