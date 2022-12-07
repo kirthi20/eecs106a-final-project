Fixed launch file for static transforms broadcaster, debugged static transforms braodcaster

ARTag_Workspace/src/automatic_navigation/src/static_transforms_broadcaster.py
@@ -16,16 +16,19 @@ if __name__ == "__main__":
 
     while not rospy.is_shutdown():
         rospy.loginfo("TRANSFORM BROADCASTER WE ****WILL**** SEND THE FIRST TRANSFORM")
-        br.sendTransform((0.0, 0.0, 0.0),
-                    (0.0, 0.0, 0.0, 1.0),
-                    rospy.Time.now(),
-                    rospy.get_param("~frames/goal_ar_frame"),
-                    rospy.get_param("~frames/base_footprint")) # CONFIRM CORRECT DIRECTION FOR TRANSFORM
+
+        # Transform between Robot AR frame tag and Base
+        br.sendTransform((0.0,0.0,0.0),
+                         tf.transformations.quaternion_from_euler(0, 0, 1),
+                         rospy.Time.now(),
+                         rospy.get_param("~frames/robot_ar_frame"),
+                         rospy.get_param("~frames/base_footprint")) # I don't know if the parameters to this quaternion are correct
+
+        # Transform between robot's base and LiDAR
         rospy.loginfo("TRANSFORM BROADCASTER WE HAVE SENT THE FIRST TRANSFORM")
-        br.sendTransform((0.0, 0.0, 0.0),
-                    (0.0, 0.0, 0.0, 1.0),
-                    rospy.Time.now(),
-                    rospy.get_param("~frames/base_footprint"),
-                    rospy.get_param("~frames/base_scan"))
-        rospy.loginfo("TRANSFORM BROADCASTER WE HAVE SENT ****BOTH**** TRANSFORMS")
+        br.sendTransform((0.0,0.0,0.0),
+                         tf.transformations.quaternion_from_euler(0, 0, 1),
+                         rospy.Time.now(),
+                         "base_footprint",
+                         "base_scan") # These are not stored on the parameter server
         rate.sleep()