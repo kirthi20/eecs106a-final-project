4e9aeee5f2a1e0d70cbbb70f65dc789c4fcf7a4c
ktay369 <ktay369@gmail.com>
2022-12-06 17:44:30 -0800

    Create fixed_tf_broadcaster.py
    transform from sensor to ar tag

---
+++ ARTag_Workspace/src/automatic_navigation/src/fixed_tf_broadcaster.py
@@ -0,0 +1,14 @@
+import roslib   
+import rospy
+import tf
+
+rospy.init_node('fixed_tf_broadcaster')
+       br = tf.TransformBroadcaster()
+       rate = rospy.Rate(10.0)
+       while not rospy.is_shutdown():
+           br.sendTransform((0.0, 0.0, 0.0),
+                            (0.0, 0.0, 0.0, 1.0),
+                            rospy.Time.now(),
+                            "self._robot_ar_frame",
+                            "self._sensor_frame")
+           rate.sleep()
