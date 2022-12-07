6e53d2335a44929105713c1cbd14b50408cd20ad
Class Account <ee106a-agf@c105-1.local>
2022-12-06 16:02:56 -0800

    Rough pathfinding

--- ARTag_Workspace/src/automatic_navigation/src/motionPlanAndSendGoal.py
+++ ARTag_Workspace/src/automatic_navigation/src/motionPlanAndSendGoal.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """
 For more information, ask Chetan/Aditya. This code isn't currently interfacing with the main robot!
 I'm working on motion planning. This is mostly sending code. Not much planning!
@@ -29,8 +30,8 @@ class MotionPlanningAndSending():
         # STAGE 1: We create the client node and the ActionClient
         
 
-        self._fixed_frame = "odom"
-        self._sensor_frame = "base_link"
+        self._fixed_frame = rospy.get_param("~frames/fixed")
+        self._sensor_frame = rospy.get_param("~frames/sensor")
         self._robot_ar_frame = "ar_marker_0"
         self._goal_ar_frame = "ar_marker_4"
         self._home_ar_frame = "ar_marker_8"
@@ -52,8 +53,19 @@ class MotionPlanningAndSending():
         # Set up the map, to be filled in at runtime 
         self.np_map = None
 
+        # Parameter server values
+        self._x_num = rospy.get_param("~x/num")
+        self._x_min = rospy.get_param("~x/min")
+        self._x_max = rospy.get_param("~x/max")
+        self._x_res = (self._x_max - self._x_min) / self._x_num # Divide by x_num?
+        self._y_num = rospy.get_param("~y/num")
+        self._y_min = rospy.get_param("~y/min")
+        self._y_max = rospy.get_param("~y/max")
+        self._y_res = (self._y_max - self._y_min) / self._y_num # Divide by y_num?
+
+
         # Set up subscriber to get the occupancy grid array
-        self._grid_topic = "/grid/map" # Parameter server in demo.launch
+        self._grid_topic = rospy.get_param("~topics/grid") # Parameter server in demo.launch
         self._grid_sub = rospy.Subscriber(self._grid_topic, 
                                         Float32MultiArray,
                                         self.PathfinderCallback,
@@ -113,31 +125,133 @@ class MotionPlanningAndSending():
             for j in range(width):
                 self.np_map[i][j] = multiarrayAccessor(i, j)
 
+        # Debugging : print out the map
         with np.printoptions(threshold=np.inf):
             print(self.np_map)
-        self.Pathfind()
+
+        path = self.Pathfind(height, width)
+        if path:
+            self.VisualizePath(path)
 
 
     # Finds a path from a start (robot) to a goal in the map
-    def Pathfind(self):
-        # TODO : Run a pathfinding algorithm on self.np_map
+    def Pathfind(self, height, width):
         # Note : self.np_map[0][0] refers to the top left 
         start = self.getRobotGridPosition()
 
         # goal = self.getGoalGridPosition()
-        goal = (15, 15) # Just a test goal for now
+        goal = (start[0] + 3, start[1] + 3) # Just a test goal for now
 
+        # Make a copy of the map
+        grid_map = np.copy(self.np_map) # This map is the occupancy grid
+        path_map = np.zeros((height, width)) # This map will purely have the path in it (nothing else)
+
+        # TODO : Run a pathfinding algorithm on self.np_map
+        # Currently using https://www.geeksforgeeks.org/check-possible-path-2d-matrix/
+        #
         
+        # directions
+        Dir = [[0,1], [0,-1], [1,0], [-1,0]]
+
+        # queue
+        q = []
+
+        # insert start
+        q.append(start)
+
+        # until queue is empty
+        while(len(q) > 0):
+            p = q[0]
+            q.pop(0)
+
+            # mark as visited
+            path_map[p[0]][p[1]] = 1
+            grid_map[p[0]][p[1]] = 1
+
+            # destination is reached
+            if(p == goal):
+                return path_map
+
+            # check in all four directions
+            for i in range(4):
+                a = p[0] + Dir[i][0]
+                b = p[1] + Dir[i][1]
+
+                # not blocked and valid
+                if(a >= 0 and b >= 0 and a < height and b < width and grid_map[a][b] == 0.5):
+                    q.append((a,b))
+        return False
         
+
+    # Given a height x width numpy array containing a path, visualize on rviz
+    def VisualizePath(self, path):
+        m = Marker()
+        m.header.stamp = rospy.Time.now()
+        m.header.frame_id = self._fixed_frame
+        m.ns = "map"
+        m.id = 0
+        m.type = Marker.CUBE_LIST
+        m.action = Marker.ADD
+        m.scale.x = self._x_res
+        m.scale.y = self._y_res
+        m.scale.z = 0.05
+
+        for ii in range(self._x_num):
+            for jj in range(self._y_num):
+                p = Point(0.0, 0.0, 0.0)
+                (p.x, p.y) = self.VoxelCenter(ii, jj)
+
+                m.points.append(p)
+
+                c = ColorRGBA()
+                c.r = 0
+                c.g = 0
+                c.b = 0
+                c.a = 0
+                if path[ii][jj] == 1:
+                    c.r = 0
+                    c.g = 100
+                    c.b = 0
+                    c.a = 0.75
+                m.colors.append(c)
+
+        self._vis_pub.publish(m)
 
     # Convert (x, y) coordinates in odom frame to grid coordinates.
     def PointToVoxel(self, x, y):
-        # TODO : replace these instance variables
-
         grid_x = int((x - self._x_min) / self._x_res)
         grid_y = int((y - self._y_min) / self._y_res)
 
         return (grid_x, grid_y)
+
+    # Get the center point (x, y) corresponding to the given voxel.
+    def VoxelCenter(self, ii, jj):
+        center_x = self._x_min + (0.5 + ii) * self._x_res
+        center_y = self._y_min + (0.5 + jj) * self._y_res
+
+        return (center_x, center_y)
+
+    # Colormap to take log odds at a voxel to a RGBA color.
+    def Colormap(self, ii, jj):
+
+        p = self.LogOddsToProbability(self._map[ii, jj])
+
+        c = ColorRGBA()
+
+        ## CUSTOM CODE : coloring square where robot is
+        if ii == self._robot_x and jj == self._robot_y:
+            c.r = 0
+            c.g = 100
+            c.b = 0
+            c.a = 0.75
+            return c
+        ##      
+
+        c.r = p
+        c.g = 0.1
+        c.b = 1.0 - p
+        c.a = 0.75
+        return c
 
 
     def getRobotGridPosition(self):
