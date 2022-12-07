f8a340dbb1f578e5e8af09c438bce191c2deb5b5
Chetan Khanna <chetan99.khanna@gmail.com>
2022-12-06 19:40:13 -0800

    Debugging static transforms broadcaster

--- ARTag_Workspace/src/automatic_navigation/src/static_transforms_broadcaster.py
+++ ARTag_Workspace/src/automatic_navigation/src/static_transforms_broadcaster.py
@@ -7,19 +7,25 @@ import rospy
 import tf
 
 if __name__ == "__main__":
+    rospy.loginfo("TRANSFORM BROADCASTER WE WILL START THE NODE")
     rospy.init_node("/static_transforms_broadcaster")
+    rospy.loginfo("TRANSFORM BROADCASTER WE HAVE STARTED THE NODE, NOW CREATING BROADCASTER OBJECT")
     br = tf.TransformBroadcaster()
+    rospy.loginfo("TRANSFORM BROADCASTER WE HAVE CREATED THE BROADCASTER OBJECT")
     rate = rospy.Rate(10.0)
 
     while not rospy.is_shutdown():
-       br.sendTransform((0.0, 0.0, 0.0),
-                        (0.0, 0.0, 0.0, 1.0),
-                        rospy.Time.now(),
-                        rospy.get_param("~frames/goal_ar_frame"),
-                        rospy.get_param("~frames/sensor"))
-       br.sendTransform((0.0, 0.0, 0.0),
-                        (0.0, 0.0, 0.0, 1.0),
-                        rospy.Time.now(),
-                        rospy.get_param("~frames/base_footprint"),
-                        rospy.get_param("~frames/base_scan"))
-       rate.sleep()
+        rospy.loginfo("TRANSFORM BROADCASTER WE ****WILL**** SEND THE FIRST TRANSFORM")
+        br.sendTransform((0.0, 0.0, 0.0),
+                    (0.0, 0.0, 0.0, 1.0),
+                    rospy.Time.now(),
+                    rospy.get_param("~frames/goal_ar_frame"),
+                    rospy.get_param("~frames/base_footprint")) # CONFIRM CORRECT DIRECTION FOR TRANSFORM
+        rospy.loginfo("TRANSFORM BROADCASTER WE HAVE SENT THE FIRST TRANSFORM")
+        br.sendTransform((0.0, 0.0, 0.0),
+                    (0.0, 0.0, 0.0, 1.0),
+                    rospy.Time.now(),
+                    rospy.get_param("~frames/base_footprint"),
+                    rospy.get_param("~frames/base_scan"))
+        rospy.loginfo("TRANSFORM BROADCASTER WE HAVE SENT ****BOTH**** TRANSFORMS")
+        rate.sleep()
